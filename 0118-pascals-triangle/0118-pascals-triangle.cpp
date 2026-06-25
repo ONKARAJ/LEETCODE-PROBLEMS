0963bb2ff8d819f332699b30380c1d660fbd9e55
ONKAR Raj <rajonkar96@gmail.com>
2026-06-25 13:43:13 +0530

    Time: 2 ms (15.8%), Space: 9.4 MB (99.41%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows){
+vector <vector<int>> traingle;
+
+for(int i=0;i<numRows;i++){
+    vector<int>row(i+1);
+    row[0]=row[i]=1;
+
+    for(int j=1;j<i;j++){
+        row[j]=traingle[i-1][j-1]+traingle[i-1][j];
+    }
+
+    traingle.push_back(row);
+}
+
+return traingle;
+
+    }
+};
