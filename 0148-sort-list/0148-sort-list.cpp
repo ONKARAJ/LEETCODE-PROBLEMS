d8fb3ce95a3afdb9dbfafb3f1f35342ce669e79d
ONKAR Raj <rajonkar96@gmail.com>
2026-06-30 13:33:23 +0530

    Time: 11 ms (82.69%), Space: 58.1 MB (67.1%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL || head->next ==NULL){
+            return head;
+        };
+        ListNode* temp=head;
+        vector<int>onkar;
+
+        while(temp !=NULL){
+            onkar.push_back(temp->val);
+            temp=temp->next;
+        }
+        sort(onkar.begin(),onkar.end());
+
+        temp=head;
+
+        for(int x : onkar){
+            temp->val=x;
+            temp=temp->next;
+        }
+        return head;
+    }
+};
