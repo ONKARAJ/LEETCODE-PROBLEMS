91af3f725f3573aaa2aa06db8a1b3430e8eef02d
ONKAR Raj <rajonkar96@gmail.com>
2026-07-09 19:31:52 +0530

    Time: 24 ms (50.96%), Space: 228.8 MB (47.4%) - LeetHub

---
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> parent, rank;
+
+    int find(int x) {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    void unite(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a == b) return;
+
+        if (rank[a] < rank[b])
+            swap(a, b);
+
+        parent[b] = a;
+
+        if (rank[a] == rank[b])
+            rank[a]++;
+    }
+
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums,
+                                      int maxDiff, vector<vector<int>>& queries) {
+
+        parent.resize(n);
+        rank.assign(n, 0);
+
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+
+        // Connect adjacent nodes
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] <= maxDiff)
+                unite(i, i - 1);
+        }
+
+        vector<bool> ans;
+
+        for (auto &q : queries) {
+            ans.push_back(find(q[0]) == find(q[1]));
+        }
+
+        return ans;
+    }
+};
