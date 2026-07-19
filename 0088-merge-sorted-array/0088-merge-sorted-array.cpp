0a69b1618baedb9dc64569fafea3dd8c0388086a
ONKAR Raj <rajonkar96@gmail.com>
2026-07-19 16:34:02 +0530

    Time: 0 ms (100%), Space: 12.5 MB (5%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -4,7 +4,8 @@ public:
         int i=m-1;
         int j=n-1;
         int k=m+n-1;
-        while(i>=0 &&j>=0){
+
+        while(i>=0&&j>=0){
             if(nums1[i]>nums2[j]){
                 nums1[k]=nums1[i];
                 i--;
@@ -16,9 +17,9 @@ public:
         }
 
         while(j>=0){
-         nums1[k]=nums2[j];
-         j--;
-         k--;
+            nums1[k]=nums2[j];
+            j--;
+            k--;
         }
     }
 };
