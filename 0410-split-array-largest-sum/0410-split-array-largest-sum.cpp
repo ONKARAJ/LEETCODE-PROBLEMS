8d251e622cae425adc606798bf0b62c0f71e6752
ONKAR Raj <rajonkar96@gmail.com>
2026-07-23 13:18:26 +0530

    Time: 0 ms (100%), Space: 10.6 MB (43.28%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-bool canship(vector<int>&weights , int days,int capacity){
-    int daysNedded=1;
-    int capacityLoad=0;
-    for(int weight : weights){
-        if(capacityLoad+weight>capacity){
-            daysNedded++;
-            capacityLoad=0;
+bool canship(vector<int>& weights,int days,int capacity){
+    int daysneeded=1;
+    int capacityload=0;
+
+    for(int weight:weights){
+        if(capacityload+weight>capacity){
+            daysneeded++;
+            capacityload=0;
         }
-        capacityLoad +=weight;
+        capacityload+=weight;
     }
-    return daysNedded<=days;
+
+    return daysneeded<=days;
 }
     int splitArray(vector<int>& nums, int k) {
         int left=*max_element(nums.begin(),nums.end());
         int right=accumulate(nums.begin(),nums.end(),0);
-        while(left < right){
+
+        while(left<right){
             int mid=left+(right-left)/2;
+
             if(canship(nums,k,mid)){
                 right=mid;
             }else{
                 left=mid+1;
             }
         }
-        return left; 
+        return left;
     }
 };
-
