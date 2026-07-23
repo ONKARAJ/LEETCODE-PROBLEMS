2fc25e1dc8b92bb07096eff941fe55f0cfccb61c
ONKAR Raj <rajonkar96@gmail.com>
2026-07-23 13:12:58 +0530

    Time: 10 ms (70.68%), Space: 35.1 MB (19.69%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-bool canship(vector<int>&weights , int days,int capacity){
-    int daysNedded=1;
-    int capacityLoad=0;
-    for(int weight : weights){
-        if(capacityLoad+weight>capacity){
-            daysNedded++;
-            capacityLoad=0;
+
+  bool canship(vector<int>& weights,int days,int capacity){
+    int daysneeded=1;
+    int capacityload=0;
+    for(int weight:weights){
+        if(capacityload+weight>capacity){
+            daysneeded++;
+            capacityload=0;
         }
-        capacityLoad +=weight;
+        capacityload +=weight;
     }
-    return daysNedded<=days;
-}
+
+    return daysneeded<=days;
+  }
     int shipWithinDays(vector<int>& weights, int days) {
-        int left=*max_element(weights.begin(),weights.end());
-        int right=accumulate(weights.begin(),weights.end(),0);
-        while(left < right){
-            int mid=left+(right-left)/2;
+        int low=*max_element(weights.begin(),weights.end());
+        int high=accumulate(weights.begin(),weights.end(),0);
+
+        while(low<high){
+            int mid=low+(high-low)/2;
+
             if(canship(weights,days,mid)){
-                right=mid;
+                high=mid;
             }else{
-                left=mid+1;
+                low=mid+1;
             }
         }
-        return left;
+        return low;
     }
 };
