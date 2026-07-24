cf6297b6240acdce388603e26267089d8b4e4c70
ONKAR Raj <rajonkar96@gmail.com>
2026-07-24 12:50:53 +0530

    Time: 3 ms (41.07%), Space: 9.5 MB (7.69%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -4,24 +4,22 @@ public:
         unordered_map<char,char>mp1;
         unordered_map<char,char>mp2;
 
-if(s.length() !=t.length()){
-    return false;
-}
         for(int i=0;i<s.length();i++){
             char c1=s[i];
             char c2=t[i];
 
             if(mp1.count(c1)){
-                if(mp1[c1] != c2){
+                if(mp1[c1]!=c2){
                     return false;
                 }
             }else{
-                if(mp2.count(c2))
-                return false;
-
-                mp1[c1]=c2;
-                mp2[c2]=c1;
+                if(mp2.count(c2)){
+                    return false;
+                }
             }
+
+            mp1[c1]=c2;
+            mp2[c2]=c1;
         }
         return true;
     }
