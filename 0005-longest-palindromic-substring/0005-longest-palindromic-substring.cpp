16190fe8530761032dd4bd315f5df0f09a4ad48e
ONKAR Raj <rajonkar96@gmail.com>
2026-07-24 15:20:01 +0530

    Time: 8 ms (69.64%), Space: 9.2 MB (99.44%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-
-        int start = 0;
-        int maxLen = 1;
-
-        for (int i = 0; i < s.size(); i++) {
-
-            // Odd length palindrome
-            int left = i;
-            int right = i;
-
-            while (left >= 0 && right < s.size() && s[left] == s[right]) {
-
-                if (right - left + 1 > maxLen) {
-                    maxLen = right - left + 1;
-                    start = left;
+        int start=0;
+        int maxlen=0;
+        for(int i=0;i<s.length();i++){
+            int left=i;
+            int right=i;
+            while(left >=0&&right<s.length()&& s[left]==s[right]){
+                if(right-left+1>maxlen){
+                    maxlen=right-left+1;
+                    start=left;
                 }
-
                 left--;
                 right++;
             }
 
-            // Even length palindrome
-            left = i;
-            right = i + 1;
+                left=i;
+                right=i+1;
 
-            while (left >= 0 && right < s.size() && s[left] == s[right]) {
-
-                if (right - left + 1 > maxLen) {
-                    maxLen = right - left + 1;
-                    start = left;
+            while(left>=0&&right<s.length()&&s[left]==s[right]){
+                if(right-left+1>maxlen){
+                    maxlen=right-left+1;
+                    start=left;
                 }
-
                 left--;
                 right++;
             }
         }
-
-        return s.substr(start, maxLen);
+        return s.substr(start,maxlen);
     }
 };
