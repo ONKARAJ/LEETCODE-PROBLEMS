ffd5dd66ffc2bbfdf3ff1be4421d437bfff63bc9
ONKAR Raj <rajonkar96@gmail.com>
2026-08-15 13:58:05 +0530

    Time: 0 ms (100%), Space: 15.1 MB (78.3%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -6,42 +6,45 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>>ans;
-        if(!root){
+        vector<vector<int>> ans;
+
+        if (!root) {
             return ans;
         }
 
-        queue<TreeNode*>q;
+        queue<TreeNode*> q;
+
         q.push(root);
-        bool left=true;
-        while(!q.empty()){
-         int n=q.size();
-         vector<int>level(n);
-         for(int i=0;i<n;i++){
-            TreeNode* node=q.front();
-            q.pop();
+        bool left = true;
+        while (!q.empty()) {
+            int n = q.size();
+            vector<int> level(n);
 
-            int idx=left?i:n-1-i;
-            level[idx]=node->val;
+            for (int i = 0; i < n; i++) {
+                TreeNode* node = q.front();
+                q.pop();
 
-            if(node->left){
-                q.push(node->left);
+                int idx = left ? i : (n - 1 - i);
+
+                level[idx] = node->val;
+
+                if (node->left) {
+                    q.push(node->left);
+                }
+
+                if (node->right) {
+                    q.push(node->right);
+                }
             }
-
-            if(node->right){
-                q.push(node->right);
-            }
-
-         }
-
-         ans.push_back(level);
-         left=!left;
+            ans.push_back(level);
+            left = !left;
         }
         return ans;
     }
